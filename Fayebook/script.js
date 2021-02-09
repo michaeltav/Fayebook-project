ced097370d1123e5c458e025c17211f7b29f29ee
michaeltav <65745355+michaeltav@users.noreply.github.com>
2021-02-09 12:18:30 -0500

    Removed excess code from JS file and corrected function of the Login button

--- Fayebook/script.js
+++ Fayebook/script.js
@@ -2,21 +2,16 @@ const btnLogin = document.querySelector('.btn-login');
 const btnCreate = document.querySelector('.btn-create');
 const loginPage = document.querySelector('.login-page');
 const mainPage = document.querySelector('.main-page');
+const right = document.querySelector('.right');
 
 const goToLoginPage = () => {
     mainPage.style.display = 'none';
     loginPage.style.display = 'grid';
 }
 
-btnLogin.addEventListener('click', (e) => {
-    if(e.target.classList[1] === 'btn-login') {
+right.addEventListener('click', e => {
+    if(e.target.classList[1] === 'main-btn') {
         goToLoginPage();
     }
-    console.log(btnLogin);
-    
-});
-
-btnCreate.addEventListener('click', (e) => {
-    console.log(btnCreate);
-
-});
+    console.log(e.target.classList[1])
+})

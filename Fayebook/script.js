d09c2d2f5e1c9112424ef1a8074a0e2234d00ee5
michaeltav <65745355+michaeltav@users.noreply.github.com>
2021-02-20 15:42:28 -0500

    Updated the class for the login button for the login page as well as gave the login button a function prompting the error box to pop up once clicked

--- Fayebook/script.js
+++ Fayebook/script.js
@@ -3,6 +3,7 @@ const btnCreate = document.querySelector('.btn-create');
 const loginPage = document.querySelector('.login-page');
 const mainPage = document.querySelector('.main-page');
 const right = document.querySelector('.right');
+const loginModal = document.querySelector('.login-modal')
 
 const goToLoginPage = () => {
     mainPage.style.display = 'none';
@@ -12,6 +13,13 @@ const goToLoginPage = () => {
 right.addEventListener('click', e => {
     if(e.target.classList[1] === 'main-btn') {
         goToLoginPage();
+    }
+    console.log(e.target.classList[1])
+})
+
+loginPage.addEventListener('click', e => {
+    if(e.target.classList[1] === 'sub-btn') {
+        loginModal.style.display = 'block';
     }
     console.log(e.target.classList[1])
 })

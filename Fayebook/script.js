94f0ee05c7ac5aa903afe48a4da8e9aac349dac2
michaeltav <65745355+michaeltav@users.noreply.github.com>
2021-02-21 12:38:34 -0500

    Updated ther modal box

--- Fayebook/script.js
+++ Fayebook/script.js
@@ -1,25 +1,25 @@
-const btnLogin = document.querySelector('.btn-login');
-const btnCreate = document.querySelector('.btn-create');
-const loginPage = document.querySelector('.login-page');
-const mainPage = document.querySelector('.main-page');
-const right = document.querySelector('.right');
-const loginModal = document.querySelector('.login-modal')
+const btnLogin = document.querySelector(".btn-login");
+const btnCreate = document.querySelector(".btn-create");
+const loginPage = document.querySelector(".login-page");
+const mainPage = document.querySelector(".main-page");
+const right = document.querySelector(".right");
+const loginModal = document.querySelector(".login-modal");
 
 const goToLoginPage = () => {
-    mainPage.style.display = 'none';
-    loginPage.style.display = 'grid';
-}
+  mainPage.style.display = "none";
+  loginPage.style.display = "grid";
+};
 
-right.addEventListener('click', e => {
-    if(e.target.classList[1] === 'main-btn') {
-        goToLoginPage();
-    }
-    console.log(e.target.classList[1])
-})
+right.addEventListener("click", (e) => {
+  if (e.target.classList[1] === "main-btn") {
+    goToLoginPage();
+  }
+  console.log(e.target.classList[1]);
+});
 
-loginPage.addEventListener('click', e => {
-    if(e.target.classList[1] === 'sub-btn') {
-        loginModal.style.display = 'block';
-    }
-    console.log(e.target.classList[1])
-})
+loginPage.addEventListener("click", (e) => {
+  if (e.target.classList[1] === "sub-btn") {
+    loginModal.style.display = "block";
+  }
+  console.log(e.target.classList[1]);
+});

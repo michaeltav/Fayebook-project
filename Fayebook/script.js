ad66c13fdf8b41c10db1da9b0c9cf41e0360f486
michaeltav <65745355+michaeltav@users.noreply.github.com>
2021-05-11 22:27:59 -0400

    Added a class to the div with the warning text as well as added a couple of const variables

--- Fayebook/script.js
+++ Fayebook/script.js
@@ -4,11 +4,18 @@ const loginPage = document.querySelector(".login-page");
 const mainPage = document.querySelector(".main-page");
 const right = document.querySelector(".right");
 const loginModal = document.querySelector(".login-modal");
+const loginWarning = document.querySelector(".login-warning");
+const loginUserInfo = document.querySelector(".login-user-info");
+const loginPassword = document.querySelector(".login-password");
 
 const goToLoginPage = () => {
   mainPage.style.display = "none";
   loginPage.style.display = "grid";
   loginModal.style.display = "block";
+};
+
+const warningMessage = () => {
+  loginWarning.style.display = "gird";
 };
 
 right.addEventListener("click", (e) => {
@@ -20,6 +27,7 @@ right.addEventListener("click", (e) => {
 
 loginModal.addEventListener("click", (e) => {
   if (e.target.classList[1] === "sub-btn") {
+    warningMessage();
   }
   console.log(e.target.classList[1]);
 });
